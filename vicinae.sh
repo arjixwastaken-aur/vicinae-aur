8b2c92791e728b581dbbccd03df2d8cd82876d54
arjix <me@arjix.dev>
2025-08-17 21:00:07 +0000

    Update vicinae.sh

--- vicinae.sh
+++ vicinae.sh
@@ -1,32 +1,31 @@
 #!/bin/sh
 
-SERVICE_NAME="vicinae"
-
 service_param() {
     local param="$1"
-    systemctl --user show -p "${param}" --value "${SERVICE_NAME}"
+    systemctl --user show -p "${param}" --value vicinae
 }
 
-if [[ "${RUNNING_AS_SERVICE}" != "1" && "$1" = "server" ]]; then
+if [[ "$RUNNING_AS_SERVICE" != "1" && "$1" = "server" ]]; then
     systemctl --user daemon-reload
 
     LOAD_STATE=$(service_param "LoadState")
     if [[ "$LOAD_STATE" != "loaded" ]]; then
-        mkdir -p "${HOME}/.config/systemd/user"
-        ln -s "/usr/lib/systemd/system/${SERVICE_NAME}.service" "${HOME}/.config/systemd/user/${SERVICE_NAME}.service"
+        mkdir -p "$HOME/.config/systemd/user"
+        ln -s "/usr/lib/systemd/system/vicinae.service" "$HOME/.config/systemd/user/vicinae.service"
         systemctl --user daemon-reload
-        systemctl --user enable "${SERVICE_NAME}"
+        systemctl --user enable "vicinae"
     fi
 
     ACTIVE_STATE=$(service_param "ActiveState")
     if [[ "${ACTIVE_STATE}" = "active" ]]; then
-        systemctl --user restart "${SERVICE_NAME}"
+        systemctl --user restart vicinae
         echo "INFO: Restarting vicinae server"
     elif [[ "${ACTIVE_STATE}" = "inactive" ]]; then
-        systemctl --user start "${SERVICE_NAME}"
+        systemctl --user start vicinae
         echo "INFO: Starting vicinae server"
     else
-        echo "ERR: Please check the server logs using: journalctl -u ${SERVCE_NAME}"
+        systemctl --user restart vicinae
+        echo "ERR: Please check the server logs using: journalctl --user -u vicinae"
         exit 1
     fi
 
